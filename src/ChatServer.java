f293d41b0d045a03aa5ec1972bda8b0993ebf4e0
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-07-28 10:02:04 +0100

    first commit

---
+++ src/ChatServer.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatServer {
+    private static Set<ClientHandler> clientHandlers = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        final int PORT = 1234;
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server Started on port " + PORT);
+
+            while ()
+        }
+    }
+}
---
+++ src/ClientHandler.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClientHandler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
